dd057c6d60761b73f545cbe758d03608e243a9d1
Kavenaa Uthayachandran <kavenaa.uthayachandran@uoit.net>
2018-11-26 21:35:37 -0500

    Update welcome.php
    added some things + logout feature

--- welcome.php
+++ welcome.php
@@ -4,12 +4,10 @@ if (!isset($_SESSION)) {
 	session_start();
 }
 
-//Query to get users information
 $query = "SELECT * FROM user WHERE UserName = '" . $_SESSION["UserName"] . "'";
 $result = mysqli_query($conn, $query);
 $userData = mysqli_fetch_assoc($result);
 
-//Sets a session variable for current user's number from the previous SQL query
 $_SESSION['UserNumber'] = $userData["UserNo"];
 
 //Sets the users pantry number if it has not yet been set
@@ -19,7 +17,6 @@ if (is_null($userData["PantryNo"])) {
 		echo "Failed to set PantryNo";
 	}
 }
-//Sets a session variable for current user's pantry number from the previous SQL query
 $_SESSION['PantryNo'] = $userData["PantryNo"];
 ?>
 <!DOCTYPE html>
@@ -27,18 +24,15 @@ $_SESSION['PantryNo'] = $userData["PantryNo"];
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
+
+h1 {
+    text-align: center;
+}
+h3 {
+    text-align: right;
 }
 
-* {box-sizing: border-box;}
-
 .bg-img {
-  /* The image used */
-  background-image: url("food.jpg");
-  min-height: 380px;
-
   /* Center and scale the image nicely */
   background-position: center;
   background-repeat: no-repeat;
@@ -76,8 +70,76 @@ body {
   color: black;
 }
 
+body {
+  background-image: url("background.jpg");
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {box-sizing: border-box;}
+
+* {box-sizing: border-box}
+.mySlides1 {display: none}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 900px;
+  position: relative;
+  margin: auto;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a grey background color */
+.prev:hover, .next:hover {
+  background-color: #f1f1f1;
+  color: black;
+}
+/* Position text in the middle of the page/image */
+.bg-text {
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  font-size: 80px;
+  border: 10px solid #f1f1f1;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 300px;
+  padding: 20px;
+text-align: center;}
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+
 </style>
 </head>
+
 <body>
 
 <div class="bg-image">
@@ -86,13 +148,53 @@ body {
 		<a href="pantry.php">Pantry</a>
 		<a href="recipes.php">Recipes</a>
 		<a href="stores.php">Stores</a>
+		<a href="logout.php">Logout</a>
 	</div>
 </div>
 
 <div style="padding-left:16px">
-  Welcome <?php echo $_SESSION["UserName"]; ?><br>
-  <p>Some content..</p>
+ <h1> Welcome <?php echo $_SESSION["UserName"]; ?>! </h1><br>
 </div>
+
+<div class="slideshow-container">
+  <div class="mySlides1">
+    <img src="pasta.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides1">
+    <img src="fish.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides1">
+    <img src="taco.jpg" style="width:100%">
+  </div>
+
+  <a class="prev" onclick="plusSlides(-1, 0)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1, 0)">&#10095;</a>
+</div>
+
+<script>
+var slideIndex = [1,1];
+var slideId = ["mySlides1"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+</script>
+
 
 </body>
 </html>
