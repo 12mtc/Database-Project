12d5721a548149a1c291dc494da6e2c9952557e8
RileyS17 <riley.stephens@uoit.net>
2018-11-27 22:18:31 -0500

    more useful dataset

--- populate_recipedb.sql
+++ populate_recipedb.sql
@@ -1,18 +1,18 @@
 INSERT INTO `foodtype` (`FoodNum`, `FoodGroup`, `FoodName`, `FoodBrand`, `Barcode`) VALUES
-(12, 'Vegetable', 'Brocoli', 'PC', '123'),
-(13, 'Fruit', 'Apple', 'NoName', '124'),
-(14, 'Dairy', 'Milk', 'Janes', '125'),
-(15, 'Grain', 'Bread', 'PC', '26'),
-(16, 'Meat', 'Chicken', 'Janes', '1232'),
-(17, 'Vegetable', 'Mushroom', 'Schnieder', '1567');
+(12, 'Vegetable', 'Brocoli', 'PC', 123),
+(13, 'Fruit', 'Apple', 'NoName', 124),
+(14, 'Dairy', 'Milk', 'Janes', 125),
+(15, 'Grain', 'Bread', 'PC', 26),
+(16, 'Meat', 'Chicken', 'Janes', 1232),
+(17, 'Vegetable', 'Mushroom', 'Schnieder', 1567);
 
-INSERT INTO `user` (`UserNo`, `UserName`, `Password`, `Email`, `Phone`, `Location`, `PantryNo`) VALUES
-(2, 'Adam', 'adam123', 'adam@yahoo.ca', 333333555, 'toronto', 2),
-(3, 'Jonas', 'jonas123', 'jonas@yahoo.ca', 234323333, 'oshawa', 3),
-(4, 'Steve', 'steve123', 'steve@gmail.com', 234321234, 'ottawa', 4),
-(5, 'Alice', 'alice123', 'alice@gmail.com', 123233243, 'windsor', 5),
-(6, 'Jill', 'jil123', 'jill@yahoo.ca', 123459876, 'london', 6),
-(7, 'Raj', 'raj123', 'raj@gmail.com', 145123489, 'quebec', 7);
+INSERT INTO `user` (`UserNo`, `UserName`, `Password`, `Email`, `Phone`, `location`, `PantryNo`) VALUES
+(2, 'Adam', 'adam123', 'adam@yahoo.ca', NULL, NULL, 2),
+(3, 'Jonas', 'jonas123', 'jonas@yahoo.ca', NULL, NULL, 3),
+(4, 'Steve', 'steve123', 'steve@gmail.com', NULL, NULL, 4),
+(5, 'Alice', 'alice123', 'alice@gmail.com', NULL, NULL, 5),
+(6, 'Jill', 'jil123', 'jill@yahoo.ca', NULL, NULL, 6),
+(7, 'Raj', 'raj123', 'raj@gmail.com', NULL, NULL, 7);
 
 INSERT INTO `recipebook` (`RecipeNum`, `AuthorNo`, `MealTime`, `RecipeName`) VALUES
 (1, 2, 'dinner', 'macaroni and cheese with bacon'),
@@ -35,19 +35,25 @@ INSERT INTO `instructions` (`RecipeNum`, `InstructionNum`, `Instruction`) VALUES
 INSERT INTO `pantry` (`PantryNo`, `FoodNum`, `Quantity`) VALUES
 (2, 12, 2),
 (3, 13, 1),
-(4, 14, 4),
+(4, 12, 55),
+(4, 13, 12),
+(4, 14, 3),
+(4, 15, 144),
+(4, 16, 5),
+(4, 17, 15),
 (5, 15, 2),
 (6, 16, 1),
 (7, 17, 2);
 
 INSERT INTO `ingredients` (`RecipeNum`, `IngredientNum`, `Quantity`) VALUES
-(1, '12', 1),
-(1, '13', 1),
-(1, '14', 1),
-(1, '15', 2),
-(4, '15', 2),
-(4, '16', 4),
-(4, '17', 1);
+(1, 12, 1),
+(1, 13, 1),
+(1, 14, 1),
+(1, 15, 2),
+(4, 15, 2),
+(4, 16, 4),
+(4, 17, 1);
+
 
 
 
